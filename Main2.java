031bc263af633d69f9a87594f20c8d2ac417da7a
TaisiaKos <106245276+TaisiaKos@users.noreply.github.com>
2022-10-27 13:13:40 +0300

    Delete Main2.java

--- Main2.java
+++
@@ -1,8 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        int prise = 100;
-        int one_mile = 20;
-        int mile = prise / one_mile;
-        System.out.println(mile);
-    }
-}
