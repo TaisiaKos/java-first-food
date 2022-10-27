3db54ed48a2c736686bb645fef6242ecc19a785f
TaisiaKos <106245276+TaisiaKos@users.noreply.github.com>
2022-10-27 13:13:30 +0300

    Delete Main3.java

--- Main3.java
+++
@@ -1,9 +0,0 @@
-public class Main3 {
-    public static void main(String[] args) {
-        int money = 1000;
-        int refill = 2000;
-        int bonus = refill / 1000;
-        System.out.println (refill + money + bonus + " Итоговый счет");
-        System.out.println (bonus + " Количество бонусов");
-    }
-}
